ff1feaf4a8e285265c59cfe0d246e8b85fb78c6e
WashingtonA01 <arianna.s.washington@gmail.com>
2025-02-02 11:31:32 -0500

    Part2 Energy Drink Consumption.cpp

--- Energy Drink Consumption.cpp
+++ Energy Drink Consumption.cpp
@@ -6,9 +6,9 @@ Requirements: A soft drink company recently surveyed 16,500 of its customers and
 percent of those surveyed purchase one or more energy drinks per week. Of those customers who purchase energy drinks,
 approximately 58 percent of them prefer citrus-flavored energy drinks. Write a program that displays the following:
 
-• The approximate number of customers in the survey who purchase one or more energy drinks per week.
+â€¢ The approximate number of customers in the survey who purchase one or more energy drinks per week.
 
-• The approximate number of customers in the survey who prefer citrus-flavored energy drinks.
+â€¢ The approximate number of customers in the survey who prefer citrus-flavored energy drinks.
 
 */ 
 
@@ -17,16 +17,28 @@ approximately 58 percent of them prefer citrus-flavored energy drinks. Write a p
 using namespace std;
 
 int main()
-{
-    int totalcustomers = 16500;
-    float percentoneormore = .15;
-    float numberofoneormore = totalcustomers * percentoneormore;
-    float percentcitrusdrinks = .58;
-    float numbercitrusdrinkers = totalcustomers * percentcitrusdrinks;
+{  
+    int totalcustomers;
+   float oneormoredrinks, prefercitrus;
+ 
+   cout << "Total number of customers surveyed:" << " ";
+   cin >> totalcustomers;
+   cout << "Number of customers in who purchase one or more energy drinks per week:" << " ";
+   cin >> oneormoredrinks;
 
-    cout << "Number of customers in who purchase one or more energy drinks per week:" << " " << numberofoneormore << endl;
-    cout << "Number of customers who refer citrus-flavored energy drinks:" << " " << numbercitrusdrinkers;
+   float percentoneormore = oneormoredrinks / 100;
+   float numberofoneormore = totalcustomers * percentoneormore;
 
+
+   cout << "Percent of customers that purchase one or more energy drinks per week:" << "" << percentoneormore << endl;
+
+   cout << "Number of customers who refer citrus-flavored energy drinks:" << " ";
+   cin >> prefercitrus;
+
+   float percentcitrusdrinks = prefercitrus / 100;
+   float numbercitrusdrinkers = totalcustomers * percentcitrusdrinks;
+
+   cout << "Percent of customers that prefer citrus-flavored energy drinks:" << " " << percentcitrusdrinks;
 
 }
 
